ace575a187f147bc10eeaaecf202d5dc55bb9bef
Derek Garnett <rizzagum@gmail.com>
2024-11-03 13:46:46 +0000

    Adds styling to Navbar removes toggleonclick

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,28 +4,28 @@ import styles from "../styles/NavBar.module.css";
 import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 const NavBar = () => {
-  const currentUser = "Marz Barz";
-  const {expanded, setExpanded, ref} = useClickOutsideToggle();
+  const currentUser = "marz Barz";
+  // const {expanded, setExpanded, ref} = useClickOutsideToggle();
   const loggedInUser = (
 
     <>
-      <Nav.Link href="#link">User: {currentUser}</Nav.Link>
+      
       <Nav className="mr-auto">
         <NavDropdown title="Take Action" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Add a Project to Track</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">View Your Marked Issues</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1"  className={styles.NavLink}>Add a Project to Track</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2"  className={styles.NavLink}>View Your Marked Issues</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3"  className={styles.NavLink}>Something</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4"  className={styles.NavLink}>Separated link</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#home"  className={styles.NavLink}>Log Out</Nav.Link>
       </Nav>
     </>
   );
   const loggedOutUser = (
     <>
       <Router>
-        {" "}
+      
         <Nav.Link
           to="/signup"
           className={styles.NavLink}
@@ -53,25 +53,27 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar bg="light" expand="xl" expanded={expanded}>
+    <div>
+    <Navbar bg="light" expand="xl" >
       <Container>
         <Navbar.Brand href="#home">
           Bug Alert<i className="fa-solid fa-crosshairs"></i>
         </Navbar.Brand>
-
-        <Navbar.Toggle ref={ref}
-          onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
+        {currentUser ? (<Nav.Link href="#link">User: {currentUser}</Nav.Link>) : ("")}
+        <Navbar.Toggle/>
         <Navbar.Collapse id="basic-navbar-nav">
           {currentUser ? loggedInUser : loggedOutUser}
           <div className="ml-auto">
-            <Form inline>
+          <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
               <Button variant="outline-success">Search</Button>
-            </Form>
+            </Form> 
           </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
+    
   );
 };
 
