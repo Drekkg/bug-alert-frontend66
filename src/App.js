6bafcd5772fb728dd25f827798542c9b9d9a3d27
Derek Garnett <rizzagum@gmail.com>
2024-11-03 00:15:13 +0000

    adds to navbar- adds projrectlist to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import NavBar from './components/NavBar';
+import ProjectList from './components/project/ProjectList';
 
 function App() {
   return (
     <div className="App">
 <NavBar />
-  
+  <ProjectList/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,10 @@ import styles from "../styles/NavBar.module.css";
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+const currentUser = "Marz Creeper";
+
+
     const loggedOutUser = (
 <>
 <Router>
@@ -36,7 +40,7 @@ const NavBar = () => {
           activeClassName={styles.Active}
           to="/posts/create"
         >
-          <i className="fa-solid fa-folder-plus"></i>Add a Project
+          <i className="fa-solid fa-folder-plus"></i>Add a Project to Track
         </NavLink>
         </Router>
       );
@@ -47,10 +51,9 @@ return (
            
                 
                 <h1>Bug Alert</h1>
-                
-       
                 {addProject}
-        
+                <h5 className="badge badge-secondary">USER: {currentUser}</h5>
+                
 
 </Container>
     </Navbar>
