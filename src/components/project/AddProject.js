3b62c1d8cd1456dd79712f81eacd11961483dadf
Derek Garnett <rizzagum@gmail.com>
2024-11-16 09:57:35 +0000

    UPDATES AddProject component to include success alert upon project addition; enhances user feedback and form handling.

--- src/components/project/AddProject.js
+++ src/components/project/AddProject.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Container, Button } from "react-bootstrap";
+import { Form, Container, Button, Alert } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import styles from "../../styles/AddProject.module.css";
 import axios from "axios";
@@ -8,7 +8,10 @@ function AddProject({ addProject }) {
   const closeForm = () => {
     history.push("/");
   };
-
+  const handleClickAlert = () => {
+    setShowAlert(false);
+    history.push("/");
+  };
   const [project, setProject] = useState({
     title: "",
     description: "",
@@ -16,6 +19,7 @@ function AddProject({ addProject }) {
     githubURL: "",
   });
   const history = useHistory();
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,14 +31,14 @@ function AddProject({ addProject }) {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("add project" + project);
+
     try {
       const response = await axios.post("/projects/", project);
-      console.log("Project added successfully", response.data);
+
+      setShowAlert(true);
     } catch (err) {
       if (err.response) {
         // Server responded with a status other than 2xx
-        console.error("Error response:", err.response.data);
       } else if (err.request) {
         // Request was made but no response received
         console.error("Error request:", err.request);
@@ -43,7 +47,6 @@ function AddProject({ addProject }) {
         console.error("Error message:", err.message);
       }
     }
-
     addProject(project);
     setProject({
       title: "",
@@ -51,80 +54,91 @@ function AddProject({ addProject }) {
       projectURL: "",
       githubURL: "",
     });
-    history.push("/");
   };
+  const addProjectAlert = (
+    <Alert variant="success">
+      <Alert.Heading>Project Deleted</Alert.Heading>
+      <p>The Project has been added successfully</p>
+      <button onClick={handleClickAlert}>Close</button>
+    </Alert>
+  );
 
   return (
     <div>
       <Container className={styles.AddProject}>
-        <h2>Add a Project</h2>
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="add-project">
-            <Form.Label>Project Title </Form.Label>
-            <Form.Text className="text-muted">
-              Please enter the name of the project you want to track.
-            </Form.Text>
-            <Form.Control
-              type="text"
-              maxLength="35"
-              onChange={handleChange}
-              placeholder="Project Name"
-              name="title"
-              value={project.title}
-            />
-          </Form.Group>
+        {showAlert && addProjectAlert}
+        {!showAlert && (
+          <div>
+            <h2>Add a Project</h2>
+            <Form onSubmit={submitHandler}>
+              <Form.Group controlId="add-project">
+                <Form.Label>Project Title </Form.Label>
+                <Form.Text className="text-muted">
+                  Please enter the name of the project you want to track.
+                </Form.Text>
+                <Form.Control
+                  type="text"
+                  maxLength="35"
+                  onChange={handleChange}
+                  placeholder="Project Name"
+                  name="title"
+                  value={project.title}
+                />
+              </Form.Group>
 
-          <Form.Group controlId="description">
-            <Form.Label>Project Description</Form.Label>
-            <Form.Text className="text-muted">
-              Please give a brief description of the application..
-            </Form.Text>
-            <Form.Control
-              type="text"
-              onChange={handleChange}
-              maxLength="150"
-              placeholder="Project Description"
-              name="description"
-              value={project.description}
-            />
-          </Form.Group>
+              <Form.Group controlId="description">
+                <Form.Label>Project Description</Form.Label>
+                <Form.Text className="text-muted">
+                  Please give a brief description of the application..
+                </Form.Text>
+                <Form.Control
+                  type="text"
+                  onChange={handleChange}
+                  maxLength="150"
+                  placeholder="Project Description"
+                  name="description"
+                  value={project.description}
+                />
+              </Form.Group>
 
-          <Form.Group controlId="projectUrl">
-            <Form.Label>Project URL</Form.Label>
-            <Form.Text className="text-muted">
-              Please paste the Project's URL: https://www.example.com
-            </Form.Text>
-            <Form.Control
-              type="text"
-              onChange={handleChange}
-              maxLength="150"
-              placeholder="Project URL"
-              name="projectURL"
-              value={project.projectURL}
-            />
-          </Form.Group>
-          <Form.Group controlId="githubUrl">
-            <Form.Label>GitHub URL</Form.Label>
-            <Form.Text className="text-muted">
-              Please paste the Project's GitHub URL: https://github.com/...
-            </Form.Text>
-            <Form.Control
-              type="text"
-              onChange={handleChange}
-              maxLength="150"
-              placeholder="Project URL"
-              name="githubURL"
-              value={project.githubURL}
-            />
-          </Form.Group>
+              <Form.Group controlId="projectUrl">
+                <Form.Label>Project URL</Form.Label>
+                <Form.Text className="text-muted">
+                  Please paste the Project's URL: https://www.example.com
+                </Form.Text>
+                <Form.Control
+                  type="text"
+                  onChange={handleChange}
+                  maxLength="150"
+                  placeholder="Project URL"
+                  name="projectURL"
+                  value={project.projectURL}
+                />
+              </Form.Group>
+              <Form.Group controlId="githubUrl">
+                <Form.Label>GitHub URL</Form.Label>
+                <Form.Text className="text-muted">
+                  Please paste the Project's GitHub URL: https://github.com/...
+                </Form.Text>
+                <Form.Control
+                  type="text"
+                  onChange={handleChange}
+                  maxLength="150"
+                  placeholder="Project URL"
+                  name="githubURL"
+                  value={project.githubURL}
+                />
+              </Form.Group>
 
-          <Button variant="success" type="submit">
-            Submit
-          </Button>
-          <Button variant="warning" className={styles.CloseButton} onClick={closeForm}>
-            Close
-          </Button>
-        </Form>
+              <Button variant="success" type="submit">
+                Submit
+              </Button>
+              <Button variant="warning" className={styles.CloseButton} onClick={closeForm}>
+                Close
+              </Button>
+            </Form>
+          </div>
+        )}
       </Container>
     </div>
   );
